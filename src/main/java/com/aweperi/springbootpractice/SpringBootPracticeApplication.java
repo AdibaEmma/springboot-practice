c5de25b3d49e64cd0488de3a0d9700c6feeadd4d
Emmanuel Adiba <eabaagah@gmail.com>
2022-03-25 10:11:23 +0000

    add student model

--- src/main/java/com/aweperi/springbootpractice/SpringBootPracticeApplication.java
+++ src/main/java/com/aweperi/springbootpractice/SpringBootPracticeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootpracticeApplication {
+public class SpringBootPracticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootpracticeApplication.class, args);
+        SpringApplication.run(SpringBootPracticeApplication.class, args);
     }
 
 }
