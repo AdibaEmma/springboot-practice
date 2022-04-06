e6a029a2987670cdc367dfa31f9311aaed7a539a
Emmanuel Adiba <eabaagah@gmail.com>
2022-04-06 16:31:47 +0000

    update user model and repository

--- src/main/java/com/aweperi/springbootpractice/model/User.java
+++ src/main/java/com/aweperi/springbootpractice/model/User.java
@@ -6,25 +6,43 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Collections;
 
 @Data
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
     @Column(name = "user_id", nullable = false)
     private Long user_id;
+    
+    @Column(name = "name")
+    @NotNull
     private String name;
+    
+    @Column(name = "username")
     private String username;
+    
+    @Column(name = "email")
+    @NotNull
     private String email;
+    
+    @Column(name = "passoword")
     private String password;
+    
+    @Column(name = "role")
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
+    
+    @Column(name = "locked")
     private Boolean locked;
+    
+    @Column(name = "enabled")
     private Boolean enabled;
 
     public User(String name, String username, String email, String password, UserRole userRole, Boolean locked, Boolean enabled) {
--- src/main/java/com/aweperi/springbootpractice/repository/UserRepository.java
+++ src/main/java/com/aweperi/springbootpractice/repository/UserRepository.java
@@ -1,14 +1,16 @@
 package com.aweperi.springbootpractice.repository;
 
 import com.aweperi.springbootpractice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<Long, User> {
+@Transactional(readOnly=true)
+public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u from User u where u.email = ?1")
     Optional<User> findByEmail(String email);
