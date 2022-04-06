a0b7a8bd5b88016cdea96339bd907b57ddaa143f
Emmanuel Adiba <eabaagah@gmail.com>
2022-04-06 16:07:42 +0000

    update web security config

--- src/main/java/com/aweperi/springbootpractice/security/config/WebSecurityConfig.java
+++ src/main/java/com/aweperi/springbootpractice/security/config/WebSecurityConfig.java
@@ -9,12 +9,14 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
+    private final BCryptPasswordEncoder bcryptPasswordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,13 +31,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider()
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
         var provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder();
+        provider.setPasswordEncoder(bcryptPasswordEncoder);
         provider.setUserDetailsService(userService);
         return provider;
     }
