f86d861f924db0dff5a572409054153e87c2539c
Emmanuel Adiba <eabaagah@gmail.com>
2022-04-06 20:38:01 +0000

    update confirmation token class

--- src/main/java/com/aweperi/springbootpractice/service/token/ConfirmationToken.java
+++ src/main/java/com/aweperi/springbootpractice/service/token/ConfirmationToken.java
@@ -1,11 +1,46 @@
 package com.aweperi.springbootpractice.service.token;
 
+import com.aweperi.springbootpractice.model.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
 public class ConfirmationToken {
-    private Long id;
+    @Id
+    @SequenceGenerator(name = "confirmation_token_sequence", sequenceName = "confirmation_token_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "confirmation_token_sequence")
+    
+    @Column(name = "token_id")
+    private Long tokenId;
+
+    @NotNull
+    @NotBlank
     private String token;
+
+    @NotNull
     private LocalDateTime createdAt;
-    private LocalDateTime expiredAt;
+
+    private LocalDateTime expiresAt;
     private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(
+            nullable = false,
+            name = "user_id"
+    )
+    private User user;
+
+    public ConfirmationToken(String token, LocalDateTime createdAt, LocalDateTime expiresAt, LocalDateTime confirmedAt, User user) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.confirmedAt = confirmedAt;
+        this.user = user;
+    }
 }
